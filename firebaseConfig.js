af4b4b496624096b39cae517d53ba4f98e170d39
Jaynee Pham <tpham.jn@gmail.com>
2017-11-22 15:16:41 -0800

    Removed extra file

--- firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export var config = {
-    apiKey: "AIzaSyBGFYxgYLW7EZsJg_H7ZJiJuKa5bVo4Dks",
-    authDomain: "secret-santa-a6c7d.firebaseapp.com",
-    databaseURL: "https://secret-santa-a6c7d.firebaseio.com",
-    projectId: "secret-santa-a6c7d",
-    storageBucket: "secret-santa-a6c7d.appspot.com",
-    messagingSenderId: "942355488424"
- };
